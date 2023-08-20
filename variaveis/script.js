3d745b89eecfc9c6e36b824fec7d2a8f14690560
Lucas Martins <lucasmartins1096@hotmail.com>
2023-08-20 14:18:32 -0300

    StarsWars

---
+++ variaveis/script.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+var nome = 'lucas';
+
+// Declarar uma variável com a sua idade
+var idade = 28;
+// Declarar uma variável com a sua comida
+
+// favorita e não atribuir valor
+var comidaFavorita
+// Atribuir valor a sua comida favorita
+var comidaFavorita = 'Peixe cozido';
+// Declarar 5 variáveis diferentes sem valores
+var serie ='Star Wars';
+cantora = 'Lana del rey';
+cor = 'Amarelo'; 
+jogo = 'God of war'
+filme = 'Call by your name';
+
+console.log(nome, idade,comidaFavorita, serie, cantora, cor, jogo, filme);
